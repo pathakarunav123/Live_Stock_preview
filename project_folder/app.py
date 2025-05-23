8f1ee3fc44a0b945f886f7cf8576c31aeaa2f3da
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2025-05-23 15:54:46 +0530

    Update app.py

--- project_folder/app.py
+++ project_folder/app.py
@@ -17,6 +17,7 @@ def index():
             chart_filename = filename
 
     return render_template('index.html', chart_filename=chart_filename, stock_name=stock_name)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port)
